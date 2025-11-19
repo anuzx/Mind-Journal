d7bf41085e4c49e6857a3b65ac189d067478f5ce
anuzx <anujparasar4266@gmail.com>
2025-11-20 01:32:27 +0530

    few changes

--- backend/src/controllers/signup.controller.ts
+++ backend/src/controllers/signup.controller.ts
@@ -1,8 +1,25 @@
-import { Request , Response } from "express"
+import { Request, Response, } from "express";
+import express from "express";
+import { User } from "../model/users.model.js";
+
+const app = express()
+app.use(express.json())
 
 
 const signUp = async (req: Request, res: Response) => {
-    
+    //TODO:ZOD AND HASH PASS
+    const { username, password } = req.body;
+    const user = await User.create({
+        username,
+        password,
+    })
+    if (user) {
+        return res
+            .status(411)
+            .json({
+                message:"user registered already"
+            })  
+    }
 }
 
 export {
