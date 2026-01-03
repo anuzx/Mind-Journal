a44d6f1faa0e44ce1e99ed596e3f9f756ef14a8c
anuzx <anujparasar4266@gmail.com>
2026-01-04 01:35:59 +0530

    zod,bcrypt

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,1 +1,8 @@
 export const DB_NAME = "MindJournal";
+const _JWT_SECRET = process.env.JWT_SECRET;
+
+if (!_JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
+export const JWT_SECRET = _JWT_SECRET
--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,70 +1,88 @@
-import express from "express";
 import type { Request, Response } from "express";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 import { UserModel } from "../model/users.model.js";
 import { ContentModel } from "../model/content.model.js";
-const JWT_SECRET = process.env.JWT_SECRET;
+import { SigninSchema, SignupSchema } from "../schema/index.js";
+import { JWT_SECRET } from "../constants.js";
 
-if (!JWT_SECRET) {
-  throw new Error("JWT_SECRET is missing in environment variables");
-}
-const app = express();
-app.use(express.json());
 
 const signUp = async (req: Request, res: Response) => {
-    //TODO:ZOD AND HASH PASS
-   // console.log("BODY:", req.body); //for debug
-    const { username, password } = req.body;
-    
-    try {
-         await UserModel.create({
-            username: username,
-            password: password,
-        });
+  //TODO:ZOD AND HASH PASS -> DONE
 
-        res.status(201).json({
-            message: "user registered successfully",
-        });
-    } catch (e) {
-        res.status(411).json({
-            message:"user already exist"
-        })
-    }
+  const parsedData = SignupSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.json({
+      message: "incoorect inputs",
+    });
+    return;
+  }
+  const hashedPassword = await bcrypt.hash(parsedData.data?.password, 10);
+
+  try {
+    await UserModel.create({
+      username: parsedData.data?.username,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "user registered successfully",
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "user already exist",
+    });
+  }
 };
 
 const signIn = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    try {
-        const existingUser = await UserModel.findOne({
-            username,
-            password
-
-        })
-        //if user exist sign the jwt and return it to the user
-        if (!existingUser) {
-          return res.status(401).json({
-            message: "Invalid username or password",
-          });
-        }
-
-        const token = jwt.sign({
-            id: existingUser._id
-        }, JWT_SECRET, {
-          expiresIn: "7d",
-        });
-
-        return res.status(200).json({
-          message: "Login successful",
-          token,
-        });
-    } catch (error) {
-         res.status(411).json({
-           message: "No such User",
-         });
+  const parsedData = SigninSchema.safeParse(req.body)
+  if (!parsedData.success) {
+    res.json({
+      message:"incoorect input"
+    })
+    return
+  }
+  try {
+    const existingUser = await UserModel.findOne({
+      username:parsedData.data.username
+    });
+    //if user exist sign the jwt and return it to the user
+    if (!existingUser) {
+      return res.status(401).json({
+        message: "Invalid username",
+      });
     }
-    
-};
 
+    const validPassword = await bcrypt.compare(parsedData.data.password, existingUser.password)
+    
+    if (!validPassword) {
+      res.status(401).json({
+        message:"invalid password"
+      })
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    res.status(411).json({
+      message: "No such User",
+    });
+  }
+};
 
 const postContent = async (req: Request, res: Response) => {
   try {
@@ -96,16 +114,14 @@ const postContent = async (req: Request, res: Response) => {
   }
 };
 
-
 const getContent = async (req: Request, res: Response) => {
-    
-    const userId = req.userId;
-    const content = await ContentModel.find({
-        userId:userId
-    }).populate("userId","username") //this will bring everything inside userId , but we dont want password etc.
-    res.json({
-        content
-    })
-}
+  const userId = req.userId;
+  const content = await ContentModel.find({
+    userId: userId,
+  }).populate("userId", "username"); //this will bring everything inside userId , but we dont want password etc.
+  res.json({
+    content,
+  });
+};
 
-export { signUp, signIn , postContent , getContent };
+export { signUp, signIn, postContent, getContent };
